fbc4ac8d69297782eea298e51b1bc26c9d969bf9
Taras Mamchits <tmamchitspc@gmail.com>
2024-05-25 12:02:12 -0700

    improved styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,29 +2,32 @@ import { SlBasket, SlMagnifier } from "react-icons/sl";
 
 function Header() {
     return (
-        <div className="flex items-center justify-between px-4 border-b h-14 bg-gray-200">
-            <div className="flex item-center space-x-2 text-sm">
-                <div className="text-lg font-bold">
-                    E-Commerce mockup
+        <div className="flex items-center justify-between border-b">
+            <div className="flex items-center justify-between px-4 h-14 w-11/12">
+                <div className="flex item-center space-x-2 text-sm">
+                    <div className="text-lg font-bold">
+                        E-Commerce mockup
+                    </div>
+                </div>
+                <div className="w-full max-w-xl ml-4">
+                    <form>
+                        <div className="relative">
+                            <div className="absolute inset-y-0 flex items-center pl-3">
+                                <SlMagnifier className="h-5 w-5 text-gray-500" />
+                            </div>
+                            <input
+                                className="pl-10 py-2 w-full border-0 shadow-none"
+                                placeholder="Search"
+                            />
+                        </div>
+                    </form>
                 </div>
             </div>
-            <div className="w-full max-w-xl ml-4">
-                <form>
-                    <div className="relative">
-                        <div className="absolute inset-y-0 flex items-center pl-3">
-                            <SlMagnifier className="h-5 w-5 text-gray-500" />
-                        </div>
-                        <input
-                            className="pl-10 py-2 w-full border-0 shadow-none"
-                            placeholder="Search"
-                        />
-                    </div>
-                </form>
-                {/* <div className="">
-                    <SlBasket />
-                </div> */}
+            <div className="mr-10">
+                <SlBasket />
             </div>
         </div>
+
     )
 }
 
