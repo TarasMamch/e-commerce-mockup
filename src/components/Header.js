88f360f7fa23ad348d7f15bc22416f10a0b5f70a
Taras Mamchits <tmamchitspc@gmail.com>
2024-05-25 11:06:16 -0700

    improved header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,27 @@ import { SlBasket, SlMagnifier } from "react-icons/sl";
 
 function Header() {
     return (
-        <div className="flex justify-between border-5 bg-cyan-200">
-            <div className="m-5">
-                E-Commerce mockup
-            </div>
-            <div className="m-5 flex w-4/12 justify-between">
-                <div className="flex items-center mx-10 w-max">
-                    <input className="w-full" />
-                    <SlMagnifier className="ml-5" />
+        <div className="flex items-center justify-between px-4 border-b h-14 bg-gray-200">
+            <div className="flex item-center space-x-2 text-sm">
+                <div className="text-lg font-bold">
+                    E-Commerce mockup
                 </div>
-                <SlBasket />
+            </div>
+            <div className="w-full max-w-xl ml-4">
+                <form>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 flex items-center pl-3">
+                            <SlMagnifier className="h-5 w-5 text-gray-500" />
+                        </div>
+                        <input
+                            className="pl-10 py-2 w-full border-0 shadow-none"
+                            placeholder="Search"
+                        />
+                    </div>
+                </form>
+                {/* <div className="">
+                    <SlBasket />
+                </div> */}
             </div>
         </div>
     )
