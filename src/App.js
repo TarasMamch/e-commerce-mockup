ce32dfc0b1217e1f5f53130fffb6ae92c452bbc2
Taras Mamchits <tmamchitspc@gmail.com>
2024-05-27 08:05:37 -0700

    set up router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
+import React from "react"
+import { RouterProvider, createBrowserRouter } from "react-router-dom"
 import Header from "./components/Header"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
 
 function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <HomePage />
+        },
+        {
+            path: "/login",
+            element: <LoginPage />
+        }
+    ])
+
     return (
-        <div>
+        <React.StrictMode>
             <Header />
-            <HomePage />
-            <LoginPage />
-        </div>
+            <RouterProvider router={router} />
+        </React.StrictMode>
     )
 }
 
