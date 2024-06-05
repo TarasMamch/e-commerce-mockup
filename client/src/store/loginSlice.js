61614161293cf67fcb67eb1b77621f866e871c9f
Taras Mamchits <tmamchitspc@gmail.com>
2024-06-05 14:05:05 -0700

    added basic code for redux

---
+++ client/src/store/loginSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const loginSlice = createSlice({
+    name: "loginStatus",
+    initialState: {
+        data: false
+    }
+})
+
+export default loginReducer = loginSlice.reducer
